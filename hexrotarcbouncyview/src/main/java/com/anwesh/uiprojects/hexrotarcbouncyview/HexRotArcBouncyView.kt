c54a2c6dd90b09e57daa152b98dd4fbad355eef7
Anwesh43 <anweshthecool0@gmail.com>
2020-02-11 11:28:45 +0530

    adding fixes

--- hexrotarcbouncyview/src/main/java/com/anwesh/uiprojects/hexrotarcbouncyview/HexRotArcBouncyView.kt
+++ hexrotarcbouncyview/src/main/java/com/anwesh/uiprojects/hexrotarcbouncyview/HexRotArcBouncyView.kt
@@ -15,12 +15,12 @@ import android.app.Activity
 
 val nodes : Int = 5
 val arcs : Int = 6
-val scGap : Float = 0.02f
+val scGap : Float = 0.02f / arcs
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4527A0")
 val backColor : Int = Color.parseColor("#BDBDBD")
-val delay : Long = 20
+val delay : Long = 5
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
