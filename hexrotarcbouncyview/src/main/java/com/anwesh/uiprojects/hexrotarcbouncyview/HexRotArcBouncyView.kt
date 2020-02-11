c922e5efaa19c6076d425b980fbb514849cd7640
Anwesh43 <anweshthecool0@gmail.com>
2020-02-11 10:22:10 +0530

    adding libraries and constants

--- hexrotarcbouncyview/src/main/java/com/anwesh/uiprojects/hexrotarcbouncyview/HexRotArcBouncyView.kt
+++ hexrotarcbouncyview/src/main/java/com/anwesh/uiprojects/hexrotarcbouncyview/HexRotArcBouncyView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.hexrotarcbouncyview
  * Created by anweshmishra on 11/02/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val arcs : Int = 6
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
